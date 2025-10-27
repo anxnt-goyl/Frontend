4654ac75c3bf5fb6e078093237059a15a21281ae
anxnt-goyl <anantg0609@gmail.com>
2025-10-27 10:57:33 +0530

    Experiment 5 updated

--- Experiment/Experiment 5/script.js
+++ Experiment/Experiment 5/script.js
@@ -1,2 +1,12 @@
-const n = prompt("Enter Your Name");
-alert("Hello, " +  n + " Welcome to my website");
+// const n = prompt("Enter Your Name");
+// console.log(n);
+// alert("Hello, " +  n + " Welcome to my website");
+// document.write("<h2>Hello, "+ n + " !</h2>");
+const p = prompt("🙏 Shreeman ji apna nam bataiyee 🙏: ");
+document.write("Swagat hai apka, priy "+ p +" ji 🙌");
+const a = prompt("Apni age bataiye :");
+if(a>=18){
+    document.writeln("<h1> 🙏 Priy mitr , "+p +". Aap vote  de sakte ho  !...🙏</h1>");
+}else{
+    document.writeln("<h1> 🙏Priy mitr , "+p +". Aap vote  de nhi sakte ho!...app chote hoo🙏 </h1>")
+}
