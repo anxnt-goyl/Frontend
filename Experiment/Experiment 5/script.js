86829809d18b024e29b1fafa531ed32df4c2ec22
anxnt-goyl <anantg0609@gmail.com>
2025-10-29 11:22:49 +0530

    updated

--- Experiment/Experiment 5/script.js
+++ Experiment/Experiment 5/script.js
@@ -3,10 +3,12 @@
 // alert("Hello, " +  n + " Welcome to my website");
 // document.write("<h2>Hello, "+ n + " !</h2>");
 const p = prompt("🙏 Shreeman ji apna nam bataiyee 🙏: ");
-document.write("Swagat hai apka, priy "+ p +" ji 🙌");
+document.writeln("Swagat hai apka, priy "+ p +" ji 🙌");
 const a = prompt("Apni age bataiye :");
 if(a>=18){
     document.writeln("<h1> 🙏 Priy mitr , "+p +". Aap vote  de sakte ho  !...🙏</h1>");
+    document.writeln( typeof p);
 }else{
     document.writeln("<h1> 🙏Priy mitr , "+p +". Aap vote  de nhi sakte ho!...app chote hoo🙏 </h1>")
+    document.writeln( typeof p);
 }
